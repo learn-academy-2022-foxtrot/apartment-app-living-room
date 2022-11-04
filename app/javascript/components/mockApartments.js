df77ff7843e00a3f751592cc45a8879cce48b992
let <kyle.tran.le@gmail.com>
2022-11-04 10:53:12 -0700

    unprotected index done

--- app/javascript/components/mockApartments.js
+++ app/javascript/components/mockApartments.js
@@ -1,42 +1,42 @@
 const apartments = [
   {
-    street: "4 Privet Drive",
-    city: "Little Whinging",
-    state: "Surrey",
-    manager: "Mr. Potter",
-    email: "potter@example.com",
+    street: "124 Conch Street",
+    city: "Bikini Bottom",
+    state: "Pacific Ocean",
+    manager: "Mr. SquarePants",
+    email: "ilovegary13@example.com",
     price: 2000,
     bedrooms: 3,
     bathrooms: 2,
-    pets: "yes",
+    pets: "Only Snails",
     image:
-      "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg"
+      "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2016_38/1160015/pineapple-home-2-today-160919.jpg"
   },
   {
-    street: "15 Yemen Road",
-    city: "Yemen",
-    state: "Yemen",
-    manager: "Mr. Bing",
-    email: "bing@example.com",
+    street: "15 Tuna West",
+    city: "Bikini Bottom",
+    state: "Pacific Ocean",
+    manager: "Ms. Cheeks",
+    email: "mouthbreathers@example.com",
     price: 1000,
-    bedrooms: 3,
-    bathrooms: 2,
-    pets: "yes",
+    bedrooms: 1,
+    bathrooms: 0,
+    pets: "Only Squirrels",
     image:
-      "https://i.pinimg.com/736x/4f/c1/ce/4fc1ce196ea1412f670d477a026ba2c6--saudi-arabia-drawing-reference.jpg"
+      "https://m.media-amazon.com/images/I/61fB3Nn7xpL._AC_SY1000_.jpg"
   },
   {
-    street: "742 Evengreen Terrace",
-    city: "Springfield",
-    state: "Any State",
-    manager: "Mr. Simpson",
-    email: "simpson@example.com",
-    price: 1000,
+    street: "123 Conch Street",
+    city: "Bikini Bottom",
+    state: "Pacific Ocean",
+    manager: "Mr. Tentacles", 
+    email: "clarinetluver@example.com",
+    price: 2001,
     bedrooms: 3,
     bathrooms: 2,
-    pets: "yes",
+    pets: "Negative",
     image:
-      "https://upload.wikimedia.org/wikipedia/en/c/ca/742_Evergreen_Terrace.png"
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/eaf9c21f-758a-4500-82dd-13a8d5fde74b/d3k1irn-f7e2c908-0ca4-4262-8186-3aef324b5796.png/v1/fill/w_1280,h_939,q_80,strp/squidward_residence_by_rebarcena_d3k1irn-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTM5IiwicGF0aCI6IlwvZlwvZWFmOWMyMWYtNzU4YS00NTAwLTgyZGQtMTNhOGQ1ZmRlNzRiXC9kM2sxaXJuLWY3ZTJjOTA4LTBjYTQtNDI2Mi04MTg2LTNhZWYzMjRiNTc5Ni5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.yWJ7xNdUpF_G38IJu38QBxoZnJB0SJJ3h44NQSw_foQ"
   }
 ]
 
