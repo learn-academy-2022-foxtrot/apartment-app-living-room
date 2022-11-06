ae38be793b0ee7bf867211f0d3bfb940ecd2129e
let <kyle.tran.le@gmail.com>
2022-11-05 17:57:31 -0700

    resolved comments

--- app/javascript/components/pages/ApartmentIndex.test.js
+++ app/javascript/components/pages/ApartmentIndex.test.js
@@ -20,38 +20,3 @@ describe("<ApartmentIndex />", () => {
   })
 })
 
-// import { render, screen } from '@testing-library/react'
-// import ApartmentIndex from './ApartmentIndex'
-// import mockApartments from "../mockApartments"
-// import {BrowserRouter} from "react-router-dom"
-
-// describe("<ApartmentIndex/>", ()=> {
-//     beforeEach(() => {
-//       render(
-//         <BrowserRouter>
-//           <CatIndex mockApartments={mockApartments} />
-//         </BrowserRouter>
-//         )
-//     })
-//     test("renders the ApartmentIndex page for the user", ()=> {
-//         console.log("cat index render")
-//         screen.debug()
-//         const element = screen.getByText("Cheap Apartments")
-//         screen.debug(element)
-//         expect(element).toBeInTheDocument()
-//     })
-
-//     test("renders cat cards", () => {
-
-//       mockApartments.forEach(apartment => {
-//         const apartmentStreet = screen.getByText(apartment.street)
-//         expect(apartmentStreet).toBeInTheDocument()
-//       })
-
-//       const apartmentImage = screen.getAllByRole('image')
-//       screen.debug(apartmentImage[0])
-//       expect(apartmentImage[0]).toBeInTheDocument()
-
-//     })
-
-// })
